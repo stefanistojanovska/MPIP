17c9c6eae5e746f9e9099c2d692cb85beda4a2e6
Stefani Stojanovska <stefani.stojanovska@students.finki.ukim.mk>
2019-12-24 18:17:33 +0100

    Pins fixed

--- MapsLab/app/src/main/java/com/mpip/mapslab/MapsActivity.java
+++ MapsLab/app/src/main/java/com/mpip/mapslab/MapsActivity.java
@@ -56,19 +56,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(this); //povik do api?
         //Init Service
         mService=Common.getGoogleAPIServie();
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             checkLocationPermissions();
         }
+
         getNearbyBanks(); //nearbyPlaces(String)
     }
 
     private void getNearbyBanks() {
         Log.d("getUrl","getNearbyBanks");
-        if(mMap!=null)
-        mMap.clear();
+        //if(mMap!=null)
+        //mMap.clear();
         String url=getUrl(latitude,longitude);
         mService.getNearbyPlaces(url).enqueue(new Callback<MyPlaces>() {
             @Override
@@ -78,6 +79,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     for(int i = 0; i<response.body().getResults().length; i++)
                     {
+                        Log.d("#",String.valueOf(i));
                         MarkerOptions markerOptions=new MarkerOptions();
                         Result googlePlace=response.body().getResults()[i];
                         double lat=googlePlace.getGeometry().getLocation().getLat();
@@ -88,6 +90,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         markerOptions.position(latLng);
                         markerOptions.title(placeName);
                         markerOptions.icon((BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                        //markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bank_icon2));
                         mMap.addMarker(markerOptions);
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
                         mMap.moveCamera(CameraUpdateFactory.zoomTo(11));
@@ -108,7 +111,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         StringBuilder url= new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
         //url.append("location="+latitude+","+longitude);
         url.append("location=41.99,21.42");
-        url.append("&radius="+5000);
+        url.append("&radius="+400);
         url.append("&type=bank");
         url.append("&key=AIzaSyDuJPKtd35G2ZGVq2YMEy1AQ9eUpZt3Umc");
         Log.d("getUrl",url.toString());
@@ -138,25 +141,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    private boolean checkLocationPermissions() {
+    private void checkLocationPermissions() {
         Log.d("getUrl","CheckLocationPermissions");
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
-        {
-           if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
-               ActivityCompat.requestPermissions(this, new String[] {
-                       Manifest.permission.ACCESS_FINE_LOCATION
-                       },MY_PERMISSION_CODE);
-           else
-           {
-               if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
-                   ActivityCompat.requestPermissions(this, new String[] {
-                           Manifest.permission.ACCESS_FINE_LOCATION
-                   },MY_PERMISSION_CODE);
-           }
-           return false;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            return;
         }
-        else return true;
+
+
     }
 
 
@@ -232,13 +225,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         latitude=location.getLatitude();
         longitude=location.getLongitude();
         LatLng latLng=new LatLng(latitude,longitude);
-        MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("My position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("My position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
         mMarker=mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
         if(mGoogleApiClient!=null)
         {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
         }
+       // getNearbyBanks();
     }
 }
